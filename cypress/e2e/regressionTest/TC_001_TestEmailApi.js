a0d8df2688b35a93f3fb5ab09ab6289df0f238a3
harbanslal1390 <harbanslal1390@gmail.com>
2024-11-06 09:59:08 +1000

    Yes commit in console.log

--- cypress/e2e/regressionTest/TC_001_TestEmailApi.js
+++ cypress/e2e/regressionTest/TC_001_TestEmailApi.js
@@ -1,5 +1,5 @@
 describe("This is test email api test suite",function(){
     it("This is the test case of test email api",function(){
-        
+        console.log("This is my first try")
     })
 })
