aff77b1e4740a3172d75aec299c9b003870e06f6
harbanslal1390 <harbanslal1390@gmail.com>
2024-11-06 10:13:14 +1000

    Second try of log

--- cypress/e2e/regressionTest/TC_001_TestEmailApi.js
+++ cypress/e2e/regressionTest/TC_001_TestEmailApi.js
@@ -1,5 +1,6 @@
 describe("This is test email api test suite",function(){
     it("This is the test case of test email api",function(){
         console.log("This is my first try")
+        console.log("This is second try")
     })
 })
